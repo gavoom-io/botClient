7e0b625170f00f1ed6b1f52964af76ae1c785779
gav <gav@gavoom.io>
2026-02-12 21:18:59 +0100

    first commit from pi

--- main.go
+++ main.go
@@ -5,9 +5,22 @@ import (
 	"log"
 	"net/url"
 	"time"
+	"fmt"
 
 	"github.com/gorilla/websocket"
 )
+type AuthMessage struct {
+	Type string `json:"type"`
+}
+
+type BotAuth struct {
+	AuthMessage
+	Password string `json:"password"`
+	Name string`json:"name"`
+	ID int `json: "id"`
+	CamID string`json: "camId"`
+	CamName string `json: "camName"`
+}
 
 type Message struct {
 	Event string      `json:"event"`
@@ -15,7 +28,7 @@ type Message struct {
 }
 
 func main() {
-	u := url.URL{Scheme: "ws", Host: "localhost:8084", Path: "/ws"}
+	u := url.URL{Scheme: "ws", Host: "192.168.0.141:47000", Path: "/ws"}
 	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
 		log.Fatal(err)
@@ -23,29 +36,32 @@ func main() {
 	defer conn.Close()
 
 	// Send register event
-	registerMsg := Message{
-		Event: "register",
-		Data: map[string]string{
-			"botId": "bot-123",
-			"token": "secret",
-		},
+	auth := BotAuth {
+		AuthMessage: AuthMessage{Type: "bot"},
+		Password: "secret",
+		Name: "Bot123",
+		ID: 123,
+		CamID: "cam123",
+		CamName: "Front Cam",
 	}
 
-	b, _ := json.Marshal(registerMsg)
-	conn.WriteMessage(websocket.TextMessage, b)
+
+	
 
 	// Periodic telemetry
 	for {
-		telemetryMsg := Message{
-			Event: "telemetry",
-			Data: map[string]interface{}{
-				"temperature": 24.5,
-				"battery":     87,
-			},
-		}
+		//telemetryMsg := Message{
+		//	Event: "telemetry",
+		//	Data: map[string]interface{}{
+		//		"temperature": 24.5,
+		//		"battery":     87,
+		//	},
+		//}
 
-		b, _ := json.Marshal(telemetryMsg)
-		conn.WriteMessage(websocket.TextMessage, b)
+		b, _ := json.Marshal(auth)
+		
+		fmt.Println(string(b))
+		conn.WriteJSON(auth)
 
 		time.Sleep(5 * time.Second)
 	}
